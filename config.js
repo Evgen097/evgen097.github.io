d3000d9a5c55053586536f0a6c2b26f6c9e46cb4
Evgen097 <k0990245853@gmail.com>
2018-11-30 12:32:08 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,29 @@
+var accessToken = 'pk.eyJ1IjoiZXZnZW4wOTciLCJhIjoiY2pvNzhhY2trMHQ0ZTNxcGFsYWt1bDcwbSJ9.jzkLA3kLYT7k62667orDog';
+var mapboxStyle = 'mapbox://styles/evgen097/cjog0faf76gvk2snva4npe2jf';
+
+
+// Default public token
+// pk.eyJ1IjoiZXZnZW4wOTciLCJhIjoiY2pvNzhhY2trMHQ0ZTNxcGFsYWt1bDcwbSJ9.jzkLA3kLYT7k62667orDog
+
+// После его установки запустите, browser-sync start --server --https и
+// у вас будет локальный сервер HTTPS.
+//
+//     Многие API требуют HTTPS, поэтому я использую Browser Sync для всей моей работы
+// за пределами Treehouse. Чтобы обойти ограничение использования ресурсов
+// Cross Origin для многих API, вы можете добавить --corsфлаг при запуске
+// Browser Sync.
+
+// …or create a new repository on the command line
+// echo "# geolocation" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git remote add origin https://github.com/Evgen097/geolocation.git
+//     git push -u origin master
+// …or push an existing repository from the command line
+// git remote add origin https://github.com/Evgen097/geolocation.git
+//     git push -u origin master
+
+
+
+
